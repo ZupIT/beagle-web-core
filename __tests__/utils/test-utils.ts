d325c4426122b0c408094e78a5186572d1204071
Raphael <raphael.soms@gmail.com>
2020-07-20 17:29:53 -0300

    Fixing test types

--- __tests__/utils/test-utils.ts
+++ __tests__/utils/test-utils.ts
@@ -90,5 +90,9 @@ export function createBeagleViewMock(custom: Partial<BeagleView> = {}): BeagleVi
     updateWithTree: jest.fn(custom.updateWithTree),
     getUrlBuilder: jest.fn(custom.getUrlBuilder || (() => ({ build: jest.fn((url => url)) }))),
     getBeagleNavigator: jest.fn(),
+    getStorage: () => ({
+      getItem: jest.fn(),
+      setItem: jest.fn(),
+    })
   }
 }
