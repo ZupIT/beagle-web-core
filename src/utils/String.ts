332fe273e001cb1b7418ffbd0a09025aa1e9c5b7
Tiago Peres França <tiagoperesfr@gmail.com>
2020-07-28 17:26:59 -0300

    fixing case for linux

---
+++ src/utils/String.ts
@@ -0,0 +1,32 @@
+/*
+  * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+
+export function removePrefix(str: string, prefix: string) {
+  return str.replace(new RegExp(`^${prefix}`), '')
+}
+
+export function addPrefix(str: string, prefix: string) {
+  return  (!str || str[0] !== prefix) ? `${prefix}${str}` : str
+}
+
+export function removeSuffix(str: string, suffix: string) {
+  return str.replace(new RegExp(`${suffix}$`), '')
+}
+
+export function capitalizeFirstLetter(str: string): string {
+  if (!str.length) return str
+  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`
+}
