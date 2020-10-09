6a20c9d9f683d4b4901e7f63f5c7b4657a9920a4
Lucas Bittencurt <65682607+lbittencurt@users.noreply.github.com>
2020-10-09 09:42:19 -0300

    Fix/replace Error with beagle custom error (#268)
    * replace Error with BeagleError at send-request file
    
    * replace Error with BeagleError at configuration file
    
    * fix error type from BeagleError to BeagleNetworkError
    
    * change native Error to Beagle Custom Error at view-content-manager
    
    * fix import order at view-content-manager, in agreement with lint rules
    
    * update at sendRequest BeagleNetworkError to use response data
    
    Co-authored-by: Tiago Peres França <tiagoperesfr@gmail.com>

--- src/service/beagle-service/configuration.ts
+++ src/service/beagle-service/configuration.ts
@@ -19,12 +19,13 @@ import defaultActionHandlers from 'action'
 import ComponentMetadata from 'metadata/parser'
 import { ExtractedMetadata } from 'metadata/types'
 import { updateMiddlewaresInConfiguration } from 'legacy/middlewares'
+import BeagleError from 'error/BeagleError'
 import { BeagleConfig, LifecycleHookMap } from './types'
 
 function checkPrefix(items: Record<string, any>) {
   mapKeys(items, (value, key: string) => {
     if (!key.startsWith('custom:') && !key.startsWith('beagle:')) {
-      throw new Error(
+      throw new BeagleError(
         `Please check your config. The ${key} is not a valid name. Yours components or actions should always start with "beagle:" if it\'s overwriting a default component or an action, "custom:" if it\'s a custom component or an action`,
       )
     }
