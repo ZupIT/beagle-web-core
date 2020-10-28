35bec9d0e584423f2aa794e1f2c43c4453749e0d
Hector <hector.custodio@zup.com.br>
2020-10-28 11:14:34 -0300

    Fixes test suites

--- __tests__/unit/beagle-view/browser-navigator.spec.ts
+++ __tests__/unit/beagle-view/browser-navigator.spec.ts
@@ -21,20 +21,22 @@ import { BeagleNavigator, NavigationType, NavigationController, HistoryState } f
 /**
  * @jest-environment jsdom
  */
-describe('Beagle View: Navigator', () => {
-  beforeEach(() => {
 
-    //@ts-ignore
-    window = { history: { state: {}, length: 0, back: jest.fn(), pushState: jest.fn() }, addEventListener: jest.fn(), removeEventListener: jest.fn() }
-    globalMocks.log.mockClear()
+beforeEach(() => {
+
+  //@ts-ignore
+  window = { history: { state: {}, length: 0, back: jest.fn(), pushState: jest.fn() }, addEventListener: jest.fn(), removeEventListener: jest.fn() }
+  globalMocks.log.mockClear()
+})
+
+
+describe('Browser Navigator', () => {
+
+  it('should create navigator', () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    expect(navigator).toEqual(expect.any(Object))
   })
 
-  describe('general behavior', () => {
-    it('should create navigator', () => {
-      const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-      expect(navigator).toEqual(expect.any(Object))
-    })
-  })
 
   it('should navigate', async () => {
     const mockHistoryState: HistoryState = {
@@ -49,8 +51,7 @@ describe('Beagle View: Navigator', () => {
     expect(window.history.pushState).toHaveBeenCalledWith(mockHistoryState, "")
   })
 
-  it(
-    'should throw error if a navigation attempts to start while another is in progress',
+  it('should throw error if a navigation attempts to start while another is in progress',
     async () => {
       const navigator = BrowserNavigator.create({}, [{ routes: [] }])
       let error: Error | undefined
@@ -65,8 +66,7 @@ describe('Beagle View: Navigator', () => {
       expect(error).toBeDefined()
       expect(error!.message).toMatch('navigation error')
       await promise
-    },
-  )
+    })
 
   it('should be able to navigate again after another navigation has succeeded', async () => {
     const mockHistoryStateA: HistoryState = {
@@ -128,9 +128,8 @@ describe('Beagle View: Navigator', () => {
     expect(error).toBeDefined()
     expect(error!.message).toMatch('navigation error')
   })
-})
 
-describe('initialization', () => {
+
   it('should start with initial navigation state if state undefined', () => {
     const initialState: HistoryState = {
       isBeagleState: true,
@@ -181,6 +180,7 @@ describe('initialization', () => {
   })
 
 
+
   describe('isEmpty', () => {
     it('should be empty when there are no stacks', () => {
       const navigator = BrowserNavigator.create({}, [{ routes: [] }])
@@ -196,125 +196,132 @@ describe('initialization', () => {
       expect(navigator.isEmpty()).toBe(true)
     })
 
-
-    describe('listeners', () => {
-      it('should run listener before changing the navigation history', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        //@ts-ignore
-        window.history.state = { route: [] }
-        const listener = jest.fn(() => {
-          expect(navigator.get()).toEqual([])
-        })
-        navigator.subscribe(listener)
-        await navigator.navigate('pushStack', { url: '/test' })
-        expect(listener).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
-      })
+  })
+})
 
 
-      it('should run multiple listeners', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        const listeners = [jest.fn(), jest.fn(), jest.fn()]
-        navigator.subscribe(listeners[0])
-        navigator.subscribe(listeners[1])
-        navigator.subscribe(listeners[2])
-        await navigator.navigate('pushStack', { url: '/test' })
 
-        expect(listeners[0]).toHaveBeenCalledTimes(1)
-        expect(listeners[1]).toHaveBeenCalledTimes(1)
-        expect(listeners[2]).toHaveBeenCalledTimes(1)
-        expect(listeners[0]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
-        expect(listeners[1]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
-        expect(listeners[2]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
-      })
 
-      it('should run multiple listeners at once and wait for all of them', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        let finishCounter = 0
-
-        const listener = async () => {
-          expect(finishCounter).toBe(0)
-          await new Promise<void>(resolve => setTimeout(resolve, 50))
-          finishCounter++
-        }
-
-        navigator.subscribe(listener)
-        navigator.subscribe(listener)
-        navigator.subscribe(listener)
-
-        await navigator.navigate('pushStack', { url: '/test' })
-        expect(finishCounter).toBe(3)
-      })
-
-      it('should unsubscribe listener', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        const listener = jest.fn()
-        const unsubscribe = navigator.subscribe(listener)
-        unsubscribe()
-        await navigator.navigate('pushStack', { url: '/test' })
-        expect(listener).not.toHaveBeenCalled()
-      })
-
-      it('should abort navigation if a listener throws an error', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        let hasError = false
-
-        navigator.subscribe(() => {
-          throw new Error()
-        })
-
-        try {
-          await navigator.navigate('pushStack', { url: '/test' })
-        } catch {
-          hasError = true
-        }
-
-        expect(navigator.get()).toEqual([])
-        expect(hasError).toBe(true)
-      })
-
-      it('should abort navigation if a listener rejects its promise', async () => {
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        let hasError = false
-
-        navigator.subscribe(() => new Promise((resolve, reject) => setTimeout(reject, 50)))
-
-        try {
-          await navigator.navigate('pushStack', { url: '/test' })
-        } catch {
-          hasError = true
-        }
-
-        expect(navigator.get()).toEqual([])
-        expect(hasError).toBe(true)
-      })
+describe('listeners', () => {
+  it('should run listener before changing the navigation history', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    //@ts-ignore
+    window.history.state = { route: [] }
+    const listener = jest.fn(() => {
+      expect(navigator.get()).toEqual([])
     })
+    navigator.subscribe(listener)
+    await navigator.navigate('pushStack', { url: '/test' })
+    expect(listener).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
+  })
 
-    describe('navigations', () => {
-      it('should pushStack', async () => {
-        const mockHistoryState: HistoryState = {
-          controllerId: undefined,
-          isBeagleState: true,
-          route: { url: 'stack3-view1' },
-          stack: 0,
-        }
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        await navigator.pushStack({ url: 'stack3-view1' })
-        expect(window.history.pushState).toHaveBeenCalledWith(mockHistoryState, "")
-      })
 
-      it('should pushView', async () => {
-        const mockHistoryState: HistoryState = {
-          controllerId: undefined,
-          isBeagleState: true,
-          route: { url: 'stack3-view1' },
-          stack: 0,
-        }
-        const navigator = BrowserNavigator.create({}, [{ routes: [] }])
-        await navigator.pushView({ url: 'stack3-view1' })
-        expect(window.history.pushState).toHaveBeenCalledWith(mockHistoryState, "")
-      })
+  it('should run multiple listeners', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    const listeners = [jest.fn(), jest.fn(), jest.fn()]
+    navigator.subscribe(listeners[0])
+    navigator.subscribe(listeners[1])
+    navigator.subscribe(listeners[2])
+    await navigator.navigate('pushStack', { url: '/test' })
+
+    expect(listeners[0]).toHaveBeenCalledTimes(1)
+    expect(listeners[1]).toHaveBeenCalledTimes(1)
+    expect(listeners[2]).toHaveBeenCalledTimes(1)
+    expect(listeners[0]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
+    expect(listeners[1]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
+    expect(listeners[2]).toHaveBeenCalledWith({ url: '/test' }, expect.anything())
+  })
+
+  it('should run multiple listeners at once and wait for all of them', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    let finishCounter = 0
+
+    const listener = async () => {
+      expect(finishCounter).toBe(0)
+      await new Promise<void>(resolve => setTimeout(resolve, 50))
+      finishCounter++
     }
 
+    navigator.subscribe(listener)
+    navigator.subscribe(listener)
+    navigator.subscribe(listener)
+
+    await navigator.navigate('pushStack', { url: '/test' })
+    expect(finishCounter).toBe(3)
+  })
+
+  it('should unsubscribe listener', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    const listener = jest.fn()
+    const unsubscribe = navigator.subscribe(listener)
+    unsubscribe()
+    await navigator.navigate('pushStack', { url: '/test' })
+    expect(listener).not.toHaveBeenCalled()
+  })
+
+  it('should abort navigation if a listener throws an error', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    let hasError = false
+
+    navigator.subscribe(() => {
+      throw new Error()
+    })
+
+    try {
+      await navigator.navigate('pushStack', { url: '/test' })
+    } catch {
+      hasError = true
+    }
+
+    expect(navigator.get()).toEqual(undefined)
+    expect(hasError).toBe(true)
+  })
+
+  it('should abort navigation if a listener rejects its promise', async () => {
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    let hasError = false
+
+    navigator.subscribe(() => new Promise((resolve, reject) => setTimeout(reject, 50)))
+
+    try {
+      await navigator.navigate('pushStack', { url: '/test' })
+    } catch {
+      hasError = true
+    }
+
+    expect(navigator.get()).toEqual(undefined)
+    expect(hasError).toBe(true)
+  })
+})
 
 
 
+
+
+
+
+describe('navigations', () => {
+  it('should pushStack', async () => {
+    const mockHistoryState: HistoryState = {
+      controllerId: undefined,
+      isBeagleState: true,
+      route: { url: 'stack3-view1' },
+      stack: 0,
+    }
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    await navigator.pushStack({ url: 'stack3-view1' })
+    expect(window.history.pushState).toHaveBeenCalledWith(mockHistoryState, "")
+  })
+
+  it('should pushView', async () => {
+    const mockHistoryState: HistoryState = {
+      controllerId: undefined,
+      isBeagleState: true,
+      route: { url: 'stack3-view1' },
+      stack: 0,
+    }
+    const navigator = BrowserNavigator.create({}, [{ routes: [] }])
+    await navigator.pushView({ url: 'stack3-view1' })
+    expect(window.history.pushState).toHaveBeenCalledWith(mockHistoryState, "")
+  })
+})
